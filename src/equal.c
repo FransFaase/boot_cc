0f549b6bacc66b7bb3485521a0da9f8b45a5fb90
Frans Faase <faase009@planet.nl>
2026-06-15 20:56:59 +0200

    equal.c: Load the files in memory after checking length

--- src/equal.c
+++ src/equal.c
@@ -1,10 +1,12 @@
-
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #ifdef __TCC_CC__
 
 char **_sys_env = 0;
+void *sys_malloc(size_t size);
+
 #include "sys_syscall.h"
 
 #define O_RDONLY 0
@@ -12,37 +14,34 @@ char **_sys_env = 0;
 #define read(fd, buf, count) SYSCALL_READ(fd, buf, count)
 #define open(pathname, mode) SYSCALL_OPEN(pathname, mode, 0777)
 #define close(fd) SYSCALL_CLOSE(fd)
+#define lseek(fd, offset, whence) SYSCALL_LSEEK(fd, offset, whence)
 
 #endif
-
-int fhgetc(int fh)
-{
-    unsigned char ch;
-    if (read(fh, &ch, 1) != 1)
-        return -1;
-    return ch;
-}
 
 int main(int argc, char *argv[])
 {
     int fh1 = open(argv[1], O_RDONLY);
     int fh2 = open(argv[2], O_RDONLY);
 
-    int result = -1;
-    for (;;)
-    {
-        int ch = fhgetc(fh1);
-        if (fhgetc(fh2) != ch)
-            break;
-        if (ch == -1)
-        {
-            result = 0;
-            break;
-        }
-    }
+    size_t size = lseek(fh1, 0, 2);
+    if (size != lseek(fh2, 0, 2))
+        return -1;
+    
+    lseek(fh1, 0, 0);
+    lseek(fh2, 0, 0);
+
+    char *content1 = sys_malloc(size);
+    char *content2 = sys_malloc(size);
+
+    read(fh1, content1, size);
+    read(fh2, content2, size);
 
     close(fh1);
     close(fh2);
     
-    return result;
+    for (int i = 0; i < size; i++)
+        if (content1[i] != content2[i])
+            return -1;
+    
+    return 0;
 }
